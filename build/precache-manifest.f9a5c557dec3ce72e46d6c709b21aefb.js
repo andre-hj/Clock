e151b0f13e842ba012bb4cb2c6cbd07d729a4bc4
andre hojda <andre.hojda@mzgroup.com>
2020-07-03 19:22:53 -0300

    buld

---
+++ build/precache-manifest.f9a5c557dec3ce72e46d6c709b21aefb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c0b9def95312062f2c3e6732e402b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e01242502024df62cef",
+    "url": "/static/css/main.192e3c64.chunk.css"
+  },
+  {
+    "revision": "e1c7dea40c11fec868e9",
+    "url": "/static/js/2.daa8f1c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.daa8f1c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e01242502024df62cef",
+    "url": "/static/js/main.b4b091da.chunk.js"
+  },
+  {
+    "revision": "26bdcf06561d08998aff",
+    "url": "/static/js/runtime-main.20afe7f9.js"
+  }
+]);
